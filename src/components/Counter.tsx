bb4d7129a5fe2066df30755c3b3d6817d507fe48
Rkthak <rkon2609@gmail.com>
2026-05-08 17:18:56 +0530

    complex form with multiple input fields

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,20 +1,56 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  const [reactions, setReactions] = useState({
-    like: 0,
-    disLike: 0,
+  const [loginMessage, setLogginMessage] = useState("");
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
   });
-  const handelLike = () => {
-    setReactions({ ...reactions, like: reactions.like + 1 });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e.target se name aur value nikaalte hain
+    const { name, value } = e.target;
+
+    setInput({ ...input, [name]: value });
   };
-  const handelDisLike = () => {
-    setReactions({ ...reactions, disLike: reactions.disLike + 1 });
+  const userName = input.email
+    .split("@")[0]
+    .replace(input.email.charAt(0), input.email.charAt(0).toUpperCase());
+
+  const message: string = `Welcome 🙋! Logged as  ${userName}`;
+  const handleFormSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLogginMessage(message);
   };
+
   return (
     <div>
-      <button onClick={handelLike}>👍 {reactions.like} </button> &nbsp;
-      <button onClick={handelDisLike}>👎 {reactions.disLike} </button>
+      <h1>🟢 Login</h1> <span> {loginMessage} </span> <br />
+      <br />
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="abc123@xyz.com"
+          value={input.email}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password 🫣"
+          value={input.password}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <br />
+        <button>Login</button>
+      </form>
     </div>
   );
 };
