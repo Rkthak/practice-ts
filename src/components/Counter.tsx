ad887119ee710b53c38ae6056604ec22d6ad5018
Rkthak <rkon2609@gmail.com>
2026-05-08 22:31:04 +0530

    useReducer alternate of useState

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,66 +1,36 @@
-import React, { useState } from "react";
+import { useReducer } from "react";
 
 const Counter = () => {
-  const [loginMessage, setLogginMessage] = useState<React.ReactNode>(null);
-  const [input, setInput] = useState({
-    email: "",
-    password: "",
-  });
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // e.target se name aur value nikaalte hain
-    const { name, value } = e.target;
-
-    setInput({ ...input, [name]: value });
+  type State = {
+    like: number;
   };
-  const userName = input.email
-    .split("@")[0]
-    .replace(input.email.charAt(0), input.email.charAt(0).toUpperCase());
+  type Action = { type: "LIKE" } | { type: "DISLIKE" } | { type: "TIP" };
 
-  const handleFormSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setLogginMessage(
-      <span className="text-gray-500">
-        Welcome 🙋! Logged as{" "}
-        <span className="text-green-500 font-bold">{userName}</span>
-      </span>,
-    );
+  const reducerFxn = (state: State, action: Action): State => {
+    switch (action.type) {
+      case "LIKE":
+        return { like: state.like + 1 };
+      case "DISLIKE":
+        return { like: state.like <= 0 ? state.like : state.like - 1 };
+      case "TIP":
+        return { like: state.like + 10 };
+      default:
+        return state;
+    }
   };
-
+  const InitialState: State = {
+    like: 0,
+  };
+  const [state, dispatch] = useReducer(reducerFxn, InitialState);
   return (
-    <div className="container p-4 m-auto">
-      <h1 className="text-green-700 text-2xl font-bold mb-2">🟢 Login</h1>{" "}
-      {loginMessage}
-      <form
-        onSubmit={handleFormSubmit}
-        className="border-2 p-4 border-gray-400 rounded mt-3"
-      >
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="abc123@xyz.com"
-          value={input.email}
-          onChange={handleInputChange}
-          required
-          className="p-2 border-2 w-full rounded mb-5 text-gray-500 font-medium focus:border-green-500 outline-none"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password 🫣"
-          value={input.password}
-          onChange={handleInputChange}
-          required
-          className="p-2 border-2 w-full rounded mb-8 text-gray-500 font-medium focus:border-green-500 outline-none"
-        />
-
-        <button className="bg-green-700 py-1 px-8 rounded cursor-pointer text-white font-semibold">
-          Login
-        </button>
-      </form>
-    </div>
+    <>
+      <h1>Like : {state.like}</h1>
+      <button onClick={() => dispatch({ type: "LIKE" })}>👍</button> &nbsp;
+      &nbsp;
+      <button onClick={() => dispatch({ type: "DISLIKE" })}>👎</button> &nbsp;
+      &nbsp;
+      <button onClick={() => dispatch({ type: "TIP" })}>🩷</button>
+    </>
   );
 };
 
